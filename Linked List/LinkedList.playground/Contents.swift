02a522c91b9e98ff8a632a854b24ccfb8c3e0703
GMartinsz <36652504+GMartinsz@users.noreply.github.com>
2019-08-14 10:50:52 -0300

    Add files via upload

---
+++ Linked List/LinkedList.playground/Contents.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+// #     ARRAYS
+//
+//var sample = ["itemA","itemB","itemC"]
+//sample.append("itemD")
+//sample.insert("itemE", at: 4)
+//
+//print(sample)
+
+// #    NODE
+//func example(){
+//    let node1 = Node(value: 1)
+//    let node2 = Node(value: 2)
+//    let node3 = Node(value: 3)
+//
+//    node1.next = node2
+//    node2.next = node3
+//
+//    print(node1)
+//
+//}
+//example()
+//
+
+// #    LINKED LIST
+
+//func LinkedPush(){
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//
+//    print(list)
+//
+//}
+//
+//Linked()
+
+//func LinkedAppend(){
+//    var list = LinkedList<Int>()
+//    list.append(1)
+//    list.append(2)
+//    list.append(3)
+//
+//    print(list)
+//
+//}
+//
+//LinkedAppend()
+
+//func LinkedInsert(){
+//
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//
+//    print("Before inserting: \(list)")
+//    var middleNode = list.node(at: 1)!
+//
+//    middleNode = list.insert(-1, after: middleNode)
+//
+//    print("After inserting: \(list)")
+//
+//}
+//
+//LinkedInsert()
+
+//func LinkedPop(){
+//    var list = LinkedList<Int>()
+//    list.append(3)
+//    list.append(2)
+//    list.append(1)
+//
+//    print(list)
+//    list.pop()
+//    print(list)
+//}
+//
+//LinkedPop()
+
+//func LinkedRemoveLast(){
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//
+//    let removedValue = list.removeLast()
+//    print(list)
+//
+//
+//
+//
+//}
+//
+//LinkedRemoveLast()
+
+//func LinkedListRemoveAt(_ index : Int){
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//    print(list)
+//    let node = list.node(at: index - 1)!
+//    list.remove(after: node)
+//
+//    print(list)
+//}
+//
+//
+//LinkedListRemoveAt(2)
